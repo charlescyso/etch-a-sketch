abaaa23acfa99a0309ce799988dc3e62c7ade124
charlescyso <charlescyso@gmail.com>
2022-05-27 14:43:21 -0700

    Add mouse over event to change cell colors, add change grid size button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,30 @@
-// Declare square divs and add squareDiv to class
-let square = document.createElement('div')
+// Create a square div and add class of squareDiv to it
+let square = document.createElement('div');
 square.classList.add('squareDiv');
 
-// Declare our grid
-let grid = document.querySelector('.grid')
+// Declare our grid variable
+let grid = document.querySelector('.grid');
 
-// Create multiple square divs by cloning
+// Clone and append multiple square div to grid by cloning
 for (i = 0; i < 256; i++) {
-    grid.appendChild(square.cloneNode(true))
-}
+    grid.appendChild(square.cloneNode(true));
+};
+
+// Declare node list of square div
+let squares = document.querySelectorAll('.squareDiv');
+
+// Add event listener for mousemove and mouseDown event on all square div
+squares.forEach(squareItem => squareItem.addEventListener('mousemove', changeColor));
+squares.forEach(squareItem => squareItem.addEventListener('mousedown', changeColor));
+
+// Function to change color only if mouse button is pressed down
+function changeColor(e) {
+    if (e.buttons == 1) {
+    e.target.style.background = 'white';
+}};
+
+// Declare grid adjust button
+let gridBtn = document.getElementById('gridBtn');
+
+// Declare userInput with user inputted grid size 
+gridBtn.addEventListener('click', () => userInput = prompt('How big, daddy?'));
